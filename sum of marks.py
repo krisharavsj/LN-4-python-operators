2120e965068590a9a865f53207ce15b3cd3207c7
krisharavsj <krisharsv@gmail.com>
2025-05-27 09:47:37 +0530

    sum of marks.py

---
+++ sum of marks.py
@@ -0,0 +1,8 @@
+print ("please enter the marks of the 4 subjects")
+math = int(input ("please enter your mark in math"))
+english = int(input ("please enter your marks in english"))
+science = int(input ("please enter your marks in science"))
+hindi = int(input ("please enter your marks in hindi"))
+sum= math+english+science+hindi
+percentage=(sum/400)*100
+print (percentage)
